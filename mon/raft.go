fd9c08e243639ef9471afc4bae5377bd19e8a536
dahefanteng <135310564@qq.com>
2018-05-17 13:36:05 +0800

    prepare to use badger to store raftlog

--- mon/raft.go
+++ mon/raft.go
@@ -3,6 +3,7 @@ package mon
 import (
 	"github.com/coreos/etcd/raft"
 	//"github.com/coreos/etcd/raft/raftpb"
+	"context"
 )
 
 type monNode struct {
@@ -19,4 +20,35 @@ func newMonNode(id int, peers []string) *monNode {
 		proposeC: make(chan string),
 	}
 	return m
+}
+
+func StartRaft() {
+	//receive propose from state machine
+	go func() {
+		for rc.proposeC != nil {
+			select {
+			case prop, ok := <-rc.proposeC:
+				if !ok {
+					rc.proposeC = nil
+				} else {
+					rc.node.Propose(context.TODO(), []byte(prop))
+				}
+			}
+		}
+	}()
+
+	//store raftlog and publish to state machine
+	ticker := time.NewTicker(100 * time.Millisecond)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				rc.node.Tick()
+			case rd := <-rc.node.Ready():
+				fmt.Println(rd)
+			case err := <-rc.transport.ErrorC:
+				fmt.Println(err)
+			}
+		}
+	}()
 }
--- storage/badger.go
+++ storage/badger.go
@@ -1,9 +1,9 @@
 package badger
 
 import (
-	"log"
-
+	pb "github.com/coreos/etcd/raft/raftpb"
 	"github.com/dgraph-io/badger"
+	"log"
 )
 
 type Badger struct {
@@ -49,4 +49,57 @@ func (b *Badger) Get(key string) (string, error) {
 		}
 	})
 	return string(value), err
+}
+
+func (b *Badger) Clear(key []byte) error {
+	if err := b.db.Update(func(txn *badger.Txn) error {
+		err := txn.Delete(key)
+		return err
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Implement raft.Storage
+
+// InitialState returns the saved HardState and ConfState information.
+func (b *Badger) InitialState() (pb.HardState, pb.ConfState, error) {
+
+}
+
+// Entries returns a slice of log entries in the range [lo,hi).
+// MaxSize limits the total size of the log entries returned, but
+// Entries returns at least one entry if any.
+func (b *Badger) Entries(lo, hi, maxSize uint64) ([]pb.Entry, error) {
+
+}
+
+// Term returns the term of entry i, which must be in the range
+// [FirstIndex()-1, LastIndex()]. The term of the entry before
+// FirstIndex is retained for matching purposes even though the
+// rest of that entry may not be available.
+func (b *Badger) Term(i uint64) (uint64, error) {
+
+}
+
+// LastIndex returns the index of the last entry in the log.
+func (b *Badger) LastIndex() (uint64, error) {
+
+}
+
+// FirstIndex returns the index of the first log entry that is
+// possibly available via Entries (older entries have been incorporated
+// into the latest Snapshot; if storage only contains the dummy entry the
+// first log entry is not available).
+func (b *Badger) FirstIndex() (uint64, error) {
+
+}
+
+// Snapshot returns the most recent snapshot.
+// If snapshot is temporarily unavailable, it should return ErrSnapshotTemporarilyUnavailable,
+// so raft state machine could know that Storage needs some time to prepare
+// snapshot and call Snapshot later.
+func (b *Badger) Snapshot() (pb.Snapshot, error) {
+
 }
